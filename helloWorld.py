bd9456a05e9c559b612be0d6037099fe0b10114e
Yasin <157622484+ISMAIELYASIN@users.noreply.github.com>
2024-03-21 21:26:48 +0000

    Delete helloWorld.py

--- helloWorld.py
+++
@@ -1,8 +0,0 @@
-value = input("Do you like using Git?")
-
-if value == "yes":
-    print ("Git is awesome!")
-elif value == "no":
-    print ("Git is rubbish!")
-else:
-    print("Please answer yes or no.")
