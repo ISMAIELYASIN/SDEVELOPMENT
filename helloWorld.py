61bbb6a0d97edb20eae9955037fe75ccdf8de8b5
Yasin <157622484+ISMAIELYASIN@users.noreply.github.com>
2024-03-21 21:21:37 +0000

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,8 @@
+value = input("Do you like using Git?")
+
+if value == "yes":
+    print ("Git is awesome!")
+elif value == "no":
+    print ("Git is rubbish!")
+else:
+    print("Please answer yes or no.")
